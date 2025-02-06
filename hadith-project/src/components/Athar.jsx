d37bf17331cd82b1234e886225060f75154c27fe
Wissem Jderi <asqr43641@gmail.com>
2025-02-06 16:03:06 +0100

    add toggle functionality to Athar component for displaying text

--- hadith-project/src/components/Athar.jsx
+++ hadith-project/src/components/Athar.jsx
@@ -1,12 +1,22 @@
+import React from "react";
 import PropTypes from "prop-types";
 
 export default function Athar(props) {
+  const [isShown, setIsShown] = React.useState(false);
+
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown);
+  }
+  console.log(isShown);
   return (
     <>
       <div className="athar">
         <hr />
-        <h3>{props.text}</h3>
+        {isShown && <h3>{props.text}</h3>}
         <h4>{props.narrator}</h4>
+        <button onClick={toggleShown}>
+          {!isShown ? "أظهر الأثر" : "اخفي الأثر"}
+        </button>
       </div>
     </>
   );
